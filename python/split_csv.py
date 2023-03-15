9858397bcc51d2696108627dcf71677a91149c6c
Paul Targosz <paul.targosz@itemis.de>
2023-03-15 13:40:15 +0100

    Update CSVs

--- python/split_csv.py
+++ python/split_csv.py
@@ -4,5 +4,13 @@ import csv
 clean_file = "../data/clean.csv"
 
 reader = csv.reader(open(clean_file ),delimiter=';')
-filtered = filter(lambda p: 'ohne Vibration am Scanner' == p[10], reader)
-csv.writer(open(r"abx.csv",'w'),delimiter=';').writerows(filtered)
+filtered = filter(lambda p: '_F' not in p[10], reader)
+csv.writer(open(r"../data/clean_filtered.csv",'w'),delimiter=';').writerows(filtered)
+
+
+
+clean_file = "../data/clean1-12.csv"
+
+reader = csv.reader(open(clean_file ),delimiter=';')
+filtered = filter(lambda p: '_F' not in p[10], reader)
+csv.writer(open(r"../data/clean1-12_filtered.csv",'w'),delimiter=';').writerows(filtered)
