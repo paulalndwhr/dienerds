75e1ff3ced8c4c12b679aa5858548e5740efe573
Paul Targosz <paul.targosz@itemis.de>
2023-03-15 14:25:51 +0100

    Winkellage + timefilter

--- python/split_csv.py
+++ python/split_csv.py
@@ -15,11 +15,18 @@ filtered = filter(lambda p: '2023-03-15T08:40:07.1943265Z' <= p[0] <= '2023-03-1
 csv.writer(open(r"../data/time_filtered.csv",'w'),delimiter=';').writerows(filtered)
 
 
+
+reader = csv.reader(open(clean_file ),delimiter=';')
+filtered = filter(lambda p: 'neue Zahn-Winkellage' in p[10], reader)
+csv.writer(open(r"../data/winkellage.csv",'w'),delimiter=';').writerows(filtered)
+
 clean_file = "../data/clean1-12.csv"
 
 reader = csv.reader(open(clean_file ),delimiter=';')
 filtered = filter(lambda p: '_F' not in p[10], reader)
 csv.writer(open(r"../data/clean1-12_filtered.csv",'w'),delimiter=';').writerows(filtered)
+
+
 
 
 
