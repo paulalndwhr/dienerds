d881ffce2abbd6d3ff70798ee7cb5628fe62a4fd
Paul Targosz <paul.targosz@itemis.de>
2023-03-15 13:54:30 +0100

    Timestamp filter

--- python/split_csv.py
+++ python/split_csv.py
@@ -7,6 +7,12 @@ reader = csv.reader(open(clean_file ),delimiter=';')
 filtered = filter(lambda p: '_F' not in p[10], reader)
 csv.writer(open(r"../data/clean_filtered.csv",'w'),delimiter=';').writerows(filtered)
 
+# time filtering example
+clean_file = "../data/clean_filtered.csv"
+
+reader = csv.reader(open(clean_file ),delimiter=';')
+filtered = filter(lambda p: '2023-03-15T08:40:07.1943265Z' <= p[0] <= '2023-03-15T09:03:17.0056279Z' , reader)
+csv.writer(open(r"../data/time_filtered.csv",'w'),delimiter=';').writerows(filtered)
 
 
 clean_file = "../data/clean1-12.csv"
@@ -14,3 +20,6 @@ clean_file = "../data/clean1-12.csv"
 reader = csv.reader(open(clean_file ),delimiter=';')
 filtered = filter(lambda p: '_F' not in p[10], reader)
 csv.writer(open(r"../data/clean1-12_filtered.csv",'w'),delimiter=';').writerows(filtered)
+
+
+
