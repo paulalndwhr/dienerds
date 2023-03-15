78cac4e915cc2a891dc543346e132b2986e3ed01
philipplndwhr <info@izwr.de>
2023-03-15 10:30:37 +0100

    introduced interactive graphs

--- python/visualize.py
+++ python/visualize.py
@@ -1,26 +1,30 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 from matplotlib.backends.backend_pdf import PdfPages
+import plotly.express as px
 
 df = pd.read_csv('../data/clean.csv', sep=';')
 
 columns = df.columns
 print(columns)
-with PdfPages('dienerds.pdf') as pdf:
-    for i in columns:
-        print(f'i = {i}')
-        for j in columns:
-        # for j in ['/Section D-D/Circle 1/D']:
-            print(f'j = {j}')
-            plt.figure(figsize=(len(columns), len(columns)))
-            plt.scatter(df[i], df[j], color='b', alpha=0.05)
-            plt.xlabel(f'{i}')
-            plt.ylabel(f'{j}')
-            plt.title(f'{i} gegen {j}')
-            plt.plot()
-            pdf.savefig()
-            plt.close
+# with PdfPages('dienerds.pdf') as pdf:
+#     for i in columns:
+#         print(f'i = {i}')
+#         for j in columns:
+#         # for j in ['/Section D-D/Circle 1/D']:
+#             print(f'j = {j}')
+#             plt.figure(figsize=(len(columns), len(columns)))
+#             plt.scatter(df[i], df[j], color='b', alpha=0.05)
+#             plt.xlabel(f'{i}')
+#             plt.ylabel(f'{j}')
+#             plt.title(f'{i} gegen {j}')
+#             plt.plot()
+#             pdf.savefig()
+#             plt.close
 
+fig = px.scatter(df, x='/Section D-D/Circle 1/D', y='/Section E-E/Circle 10/D', color='nr')
+fig.show()
+fig.write_html("../data/example.html")
 # import datetime
 # import numpy as np
 # from matplotlib.backends.backend_pdf import PdfPages
