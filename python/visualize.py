fb946a1b550046878ca71f65834087b7493b19b4
philipplndwhr <info@izwr.de>
2023-03-15 16:22:36 +0100

    curve_fitting works

--- python/visualize.py
+++ python/visualize.py
@@ -5,11 +5,11 @@ import plotly.express as px
 import scipy as sp
 import seaborn as sns
 import numpy as np
+from typing import List
+from math import exp
 
-df = pd.read_csv('../data/clean.csv', sep=';')
 
-columns = df.columns
-print(columns)
+
 # with PdfPages('dienerds.pdf') as pdf:
 #     for i in columns:
 #         print(f'i = {i}')
@@ -44,40 +44,159 @@ print(columns)
 # Nun dasselbe Spiel in Seaborn...
 # =====================
 
-with PdfPages('dienerds.pdf') as pdf:
-    for i in columns:
-        print(f'i = {i}')
-        for j in columns:
-        # for j in ['/Section D-D/Circle 1/D']:
-            print(f'j = {j}')
-            try:
-                x_mean = df[i].mean()
-                y_mean = df[j].mean()
-                x_var = df[i].var()
-                y_var = df[j].var()
-
-                fig, ax = plt.subplots(figsize=(10, 6))
-                ax = sns.regplot(x=df[i], y=df[j], ci=95, order=1,
-                                 # plot_kws={'line_kws': {'color': 'red', 'alpha': 1}, 'scatter_kws': {'alpha': 0.1}},
-                                 # line_kws={'label': 'Linear regression line: $Y(X)=5.74+2.39\cdot 10^{-5} X$', 'color': 'm'},
-                                 seed=1, truncate=False)  # , label="Original data")
-                ax.legend(loc="upper left")
-                r, p = sp.stats.pearsonr(df[i], df[j])
-                plt.xlabel(f'{i}, mean={x_mean}, var={x_var}\n r^2={r**2}')
-                plt.ylabel(f'{j}, mean={y_mean}, var={y_var}')
-            except TypeError:
-                x_mean = 'N/A'
-                y_mean = 'N/A'
-                x_var = 'N/A'
-                y_var = 'N/A'
-                plt.scatter(df[i], df[j], color='b', alpha=0.05)
-
-                plt.xlabel(f'{i}, mean={x_mean}, var={x_var}')
-                plt.ylabel(f'{j}, mean={y_mean}, var={y_var}')
-            plt.title(f'{i} gegen {j}')
-            plt.plot()
-            pdf.savefig()
-            plt.close
-
+# with PdfPages('dienerds-without-garbage.pdf') as pdf:
+#     for i in columns:
+#         print(f'i = {i}')
+#         for j in columns:
+#         # for j in ['/Section D-D/Circle 1/D']:
+#             print(f'j = {j}')
+#             # this section skips unnecessary diagrams which just increase the .pdf file size
+#             if i == j:
+#                 continue
+#             elif i == 'time':
+#                 continue
+#             elif j == 'time':
+#                 continue
 #
-# plt.show()
+#             try:
+#                 x_mean = df[i].mean()
+#                 y_mean = df[j].mean()
+#                 x_var = df[i].var()
+#                 y_var = df[j].var()
+#
+#                 fig, ax = plt.subplots(figsize=(10, 6))
+#                 ax = sns.regplot(x=df[i], y=df[j], ci=95, order=1,
+#                                  # plot_kws={'line_kws': {'color': 'red', 'alpha': 1}, 'scatter_kws': {'alpha': 0.1}},
+#                                  # line_kws={'label': 'Linear regression line: $Y(X)=5.74+2.39\cdot 10^{-5} X$', 'color': 'm'},
+#                                  seed=1, truncate=False)  # , label="Original data")
+#                 ax.legend(loc="upper left")
+#                 r, p = sp.stats.pearsonr(df[i], df[j])
+#                 plt.xlabel(f'{i}, mean={x_mean}, var={x_var}\n r^2={r**2}')
+#                 plt.ylabel(f'{j}, mean={y_mean}, var={y_var}')
+#             except TypeError:
+#                 x_mean = 'N/A'
+#                 y_mean = 'N/A'
+#                 x_var = 'N/A'
+#                 y_var = 'N/A'
+#                 plt.scatter(df[i], df[j], color='b', alpha=0.05)
+#
+#                 plt.xlabel(f'{i}, mean={x_mean}, var={x_var}')
+#                 plt.ylabel(f'{j}, mean={y_mean}, var={y_var}')
+#             plt.title(f'{i} gegen {j}')
+#             plt.plot()
+#             pdf.savefig()
+#             plt.close
+
+
+def pdf_potentially_relevant_diagr(df: pd.DataFrame, savename: str = 'dienerds-without-garbage.pdf') -> None:
+    """
+    draws all potentially relevant diagrams into a .pdf, using seaborn and seaborn's regression fit.
+    :param df:
+    :param savename:
+    :return:
+    """
+
+    with PdfPages(savename) as pdf:
+        for i in columns:
+            print(f'i = {i}')
+            for j in columns:
+                # for j in ['/Section D-D/Circle 1/D']:
+                print(f'j = {j}')
+                # this section skips unnecessary diagrams which just increase the .pdf file size
+                if i == j:
+                    continue
+                elif i == 'time':
+                    continue
+                elif j == 'time':
+                    continue
+
+                try:
+                    x_mean = df[i].mean()
+                    y_mean = df[j].mean()
+                    x_var = df[i].var()
+                    y_var = df[j].var()
+
+                    fig, ax = plt.subplots(figsize=(10, 6))
+                    ax = sns.regplot(x=df[i], y=df[j], ci=95, order=1,
+                                     # plot_kws={'line_kws': {'color': 'red', 'alpha': 1}, 'scatter_kws': {'alpha': 0.1}},
+                                     # line_kws={'label': 'Linear regression line: $Y(X)=5.74+2.39\cdot 10^{-5} X$', 'color': 'm'},
+                                     seed=1, truncate=False)  # , label="Original data")
+                    ax.legend(loc="upper left")
+                    r, p = sp.stats.pearsonr(df[i], df[j])
+                    plt.xlabel(f'{i}, mean={x_mean}, var={x_var}\n r^2={r ** 2}')
+                    plt.ylabel(f'{j}, mean={y_mean}, var={y_var}')
+                except TypeError:
+                    x_mean = 'N/A'
+                    y_mean = 'N/A'
+                    x_var = 'N/A'
+                    y_var = 'N/A'
+                    plt.scatter(df[i], df[j], color='b', alpha=0.05)
+
+                    plt.xlabel(f'{i}, mean={x_mean}, var={x_var}')
+                    plt.ylabel(f'{j}, mean={y_mean}, var={y_var}')
+                plt.title(f'{i} gegen {j}')
+                plt.plot()
+                pdf.savefig()
+                plt.close
+
+
+def exponential(x, a, b, c):
+    return a * np.exp(x-b) + c
+
+
+def quadratic(x, a, b, c):
+    return a * x ** 2 + b * x + c
+
+
+def cubic(x, a, b, c, d):
+    return a * x ** 3 + b * x ** 2 + c * x + d
+
+
+def bellcurve(x, a, mu, sigma, offset):
+    return offset + a * np.exp(-((x - mu) / sigma) ** 2)
+
+
+def quartic(x, a, b, c, d, e):
+    return a * x ** 4 + b * x ** 3 + c * x ** 2 + d * x + e
+
+
+def mean_square_error(function, arg_opt: List[float], xdata: List[float], ydata: List[float]) -> float:
+    residuals = ydata - function(xdata, *arg_opt)
+    ss_res = np.sum(residuals ** 2)
+    ss_tot = np.sum((ydata - np.mean(ydata)) ** 2)
+    r_squared = 1 - (ss_res / ss_tot)
+    return r_squared
+
+
+def find_best_curve(df: pd.DataFrame, col_x: str, col_y: str) -> str:
+    # popt_bell, pcov_bell = sp.optimize.curve_fit(bellcurve, df[col_x].values, df[col_y].values, p0=[1, 0, 2, 0])
+    popt_exp, pcov_exp = sp.optimize.curve_fit(exponential, df[col_x].values, df[col_y].values)
+    popt_quad, pcov_quad = sp.optimize.curve_fit(quadratic, df[col_x].values, df[col_y].values, p0=[1, 1, 1])
+    popt_cube, pcov_cube = sp.optimize.curve_fit(cubic, df[col_x].values, df[col_y].values, p0=[1, 1, 1, 1])
+    popt_quart, pcov_quart = sp.optimize.curve_fit(quartic, df[col_x].values, df[col_y].values, p0=[1, 1, 1, 1, 1])
+    # print(popt_bell)
+    print(f'exp {popt_exp}, '
+          f'r^2={mean_square_error(exponential, arg_opt=popt_exp, xdata=df[col_x].values, ydata=df[col_y].values)}')
+    print(popt_quad)
+    print(
+        f'quad {popt_quad}, '
+        f'r^2={mean_square_error(quadratic, arg_opt=popt_quad, xdata=df[col_x].values, ydata=df[col_y].values)}')
+    print(popt_cube)
+    print(
+        f'cube {popt_cube}, '
+        f'r^2={mean_square_error(cubic, arg_opt=popt_cube, xdata=df[col_x].values, ydata=df[col_y].values)}')
+    print(popt_quart)
+    print(
+        f'quartic {popt_quart}, '
+        f'r^2={mean_square_error(quartic, arg_opt=popt_quart, xdata=df[col_x].values, ydata=df[col_y].values)}')
+
+
+if __name__ == '__main__':
+    df = pd.read_csv('../data/clean.csv', sep=';')
+    columns = df.columns
+    print(columns)
+
+    find_best_curve(df=df, col_x='/Section E-E/Circle 10/D', col_y='/Section D-D/Circle 10/D')
+
+
+
